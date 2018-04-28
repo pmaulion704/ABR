7ecd2d9557dde2516d107bd87fd50fa91db45f49
Patrick <patrick.dionisio.maulion@gmail.com>
2018-04-28 00:03:16 -0400

    close feature
    created close feature once you open race details.

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ $(function (){
 			"latitude"  : 40.793973,
 			"zoom"		: 13.45,
 			"bearing"	: 65.00
-
 		},
 		{
 			"city" 		: "BRONX, NY",
@@ -37,153 +36,193 @@ $(function (){
 		// MANHATTAN
 			[
 				{
-				"id"    : "015",
-				"pace" : "8:55",
-				"overall" : 1,
-				"race" : "2017 NYRR Midnight Run",
-				"time" : "0:31:45"
+					"id"    : "015",
+					"race" : "2017 NYRR Midnight Run",
+					"time" : "0:31:45",
+					"pace" : "07:57",
+					"overall" : 259,
+					"age-place" : 321,
+					"age-percentile": "53.23%"			
 				},
 				{
-				"id"    : "014",
-				"pace" : "7:54",
-				"overall" : 12,
-				"race" : "TCS New York City Marathon", 
-				"time" : "5:05:23"
+					"id"    : "014",
+					"race" : "TCS New York City Marathon", 
+					"time" : "5:05:23",
+					"pace" : "11:39",
+					"overall" : 37062,
+					"age-place" : 25851,
+					"age-percentile": "40.26%"	
 				},
 				{
-				"id"    : "013",
-				"pace" : "6:53",
-				"overall" : 123,
-				"race" : "2017 Poland Spring Kickoff (5M)", 
-				"time" : "0:39:12"
+					"id"    : "013",
+					"race" : "2017 Poland Spring Kickoff (5M)", 
+					"time" : "0:39:12",
+					"pace" : "07:59",
+					"overall" : 609,
+					"age-place" : 789,
+					"age-percentile": "54.39%"
 				},
 				{
-				"id"    : "012",
-				"pace" : "5:52",
-				"overall" : 1234,
-				"race" : "2017 Run as One by JP Morgan Chase",
-				"time" : "0:49:31"
+					"id"    : "012",
+					"race" : "2017 Run as One by JP Morgan Chase",
+					"time" : "0:49:31",
+					"pace" : "12.23",
+					"overall" : 7576,
+					"age-place" : 4211,
+					"age-percentile": "34.13%"	
 				},
 				{
-				"id"    : "011",
-				"pace" : "zzzzzzz",
-				"overall" : "yyyyyyy",
-				"race" : "2017 UAE Healthy Kidney 10K",
-				"time" : "0:50:12"
+					"id"    : "011",
+					"race" : "2017 UAE Healthy Kidney 10K",
+					"time" : "0:50:12",
+					"pace" : "08:05",
+					"overall" : 2264,
+					"age-place" : 2364,
+					"age-percentile": "53.23%"
 				},
 				{
-				"id"    : "010",
-				"pace" : "aaaaaa",
-				"overall" : "bbbbbbbbb",
-				"race" : "2017 United Airlines NYC Half",
-				"time" : "1:54:02"
+					"id"    : "010",
+					"race" : "2017 United Airlines NYC Half",
+					"time" : "1:54:02",
+					"pace" : "08:42",
+					"overall" : 6114,
+					"age-place" : 5261,
+					"age-percentile": "51.20%"
 				},
 				{
-				"id"    : "009",
-				"pace" : "8:55",
-				"overall" : 123,
-				"race" : "2017 NYRR Gridiron 4M",
-				"time" : "0:31:53"
+					"id"    : "009",
+					"race" : "2017 NYRR Gridiron 4M",
+					"time" : "0:31:53",
+					"pace" : "07:59",
+					"overall" : 1264,
+					"age-place" : 1423,
+					"age-percentile": "53.02%"
 				},
 				{
-				"id"    : "008",
-				"pace" : "8:55",
-				"overall" : 123,
-				"race" : "2017 NYRR Joe Kleinerman 10K",
-				"time" : "0:50:51"
+					"id"    : "008",
+					"race" : "2017 NYRR Joe Kleinerman 10K",
+					"time" : "0:50:51",
+					"pace" : "08:11",
+					"overall" : 1270,
+					"age-place" : 1451,
+					"age-percentile": "52.54%"
 				},
 				{
-				"id"    : "007",
-				"pace" : "8:55",
-				"overall" : 123,
-				"race" : "2016 Poland Spring Marathon Kickoff (5M)",
-				"time" : "0:40:56"
+					"id"    : "007",
+					"race" : "2016 Poland Spring Marathon Kickoff (5M)",
+					"time" : "0:40:56",
+					"pace" : "08:12",
+					"overall" : 1012,
+					"age-place" : 1113,
+					"age-percentile": "52.09%"
 				},
 				{
-				"id"    : "006",
-				"pace" : "8:55",
-				"overall" : 123, 
-				"race" : "2016 Percy Sutton Harlem 5K Run",
-				"time" : "0:24:26"
+					"id"    : "006",
+					"race" : "2016 Percy Sutton Harlem 5K Run",
+					"time" : "0:24:26",
+					"pace" : "07:52",
+					"overall" : 1369,
+					"age-place" : 1352,
+					"age-percentile": "53.17%" 
 				},
 				{
-				"id"    : "005",
-				"pace" : "8:55",
-				"overall" : 123,	 
-				"race" : "2016 France Run 8K",
-				"time" : "0:43:47"
+					"id"    : "005",
+					"race" : "2016 France Run 8K",
+					"time" : "0:43:47",
+					"pace" : "08:49",
+					"overall" : 1548,
+					"age-place" : 1437,
+					"age-percentile": "48.42%"	 
 				},
 				{
-				"id"   : "004",
-				"pace" : "8:55",
-				"overall" : 123,		 
-				"race" : "2016 Boomer's Cystic Fibrosis Run to Breathe",
-				"time" : "0:34:20",
+					"id"   : "004",
+					"race" : "2016 Boomer's Cystic Fibrosis Run to Breathe",
+					"time" : "0:34:20",
+					"pace" : "08:35",
+					"overall" : 1553,
+					"age-place" : 1501,
+					"age-percentile": "49.23%"		 
 				},
 				{
-				"id"    : "003",
-				"pace" : "8:55",
-				"overall" : 123, 
-				"race" : "2016 Achilles Hope & Possibility (4M)",
-				"time" : "0:33:25",
+					"id"    : "003",
+					"race" : "2016 Achilles Hope & Possibility (4M)",
+					"time" : "0:33:25",
+					"pace" : "08:22",
+					"overall" : 1379,
+					"age-place" : 1437,
+					"age-percentile": "50.60%"
 				},
 				{
-				"id"    : "002",
-				"pace" : "8:55",
-				"overall" : 123,
-				"race" : "2016 UAE Health Kidney 10K",
-				"time" : "0:55:47",
+					"id"    : "002",
+					"race" : "2016 UAE Health Kidney 10K",
+					"time" : "0:55:47",
+					"pace" : "08:59",
+					"overall" : 3041,
+					"age-place" : 2692,
+					"age-percentile": "47.91%"
 				},
 				{
-				"id"   : "001",
-				"pace" : "8:55",
-				"overall" : 123,
-				"race" : "2016 City Parks Foundation Run for the Parks (4M)",
-				"time" : "0:34:16"
+					"id"   : "001",
+					"race" : "2016 City Parks Foundation Run for the Parks (4M)",
+					"time" : "0:34:16",
+					"pace" : "08:34",
+					"overall" : 2670,
+					"age-place" : 2385,
+					"age-percentile": "49.33%"
 				}	
 			],
 			// BRONX
 			[
 				{
-				"id"   : "002",
-				"pace" : "8:55",
-				"overall" : 123,
-				"race" : "2017 Bronx 10 Mile",
-				"time" : '1:24:06' 
+					"id"   : "002",
+					"race" : "2017 Bronx 10 Mile",
+					"time" : "1:24:06",
+					"pace" : "08:25",
+					"overall" : 3291,
+					"age-place" : 3362,
+					"age-percentile": "52.23%"
 				},
 				{
-				"id"   : "001",
-				"pace" : "8:55",
-				"overall" : 123,		 
-				"race" : "2016 Bronx 10 Mile",
-				"time" : '1:24:10'
+					"id"   : "001",
+					"race" : "2016 Bronx 10 Mile",
+					"time" : '1:24:10',
+					"pace" : "08:25",
+					"overall" : 3435,
+					"age-place" : 3280,
+					"age-percentile": "52.29%"		 
 				}
 			],
 			// NEW BRUNSWICK
 			[
 				{
-				"id"   : "002",
-				"pace" : "8:5523232",
-				"overall" : 1231232,
-				"race" : "2018 Rutgers UNITED Half Marathon",
-				"time" : "1:58:20"
+					"id"   : "002",
+					"race" : "2018 Rutgers UNITED Half Marathon",
+					"time" : "1:59:01",
+					"pace" : "09:04",
+					"overall" : 769,
+					"age-place" : 50,
+					"age-percentile": "- -"
 				},
 				{
-				"id"   :  "001",
-				"pace" : "0.00000",
-				"overall" : 987236236,
-				"race" : "2017 Big Chill 5K",
-				"time" : "- -"
+					"id"   :  "001",
+					"race" : "2017 Big Chill 5K",
+					"time" : "- -",
+					"pace" : "- -",
+					"overall" : "- -",
+					"age-place" : "- -",
+					"age-percentile": "- -"
 				}
 			],
 			// STATEN ISLAND
 			[
 				{
-				"id"   : "001",
-				"pace" : "8:55",
-				"overall" : 123,
-				"race" : "2017 NYRR Staten Island Half",
-				"time" : "1:53:19"
+					"id"   : "001",
+					"race" : "2017 NYRR Staten Island Half",
+					"time" : "1:53:19",
+					"pace" : "08:39",
+					"overall" : 2512,
+					"age-place" : 2599,
+					"age-percentile": "51.53%"
 				}
 			]
 		// End of INDEX
@@ -219,6 +258,7 @@ $(function (){
 			flyLocation(locations[myIndex]["longitude"], locations[myIndex]["latitude"], locations[myIndex]["zoom"], locations[myIndex]["bearing"]);
 			$("tr").remove();
 			loadData(data[myIndex], "id", "race", "time");
+			$(".sub-section").hide();
 
 		} else {
 			myIndex = 0;
@@ -226,6 +266,7 @@ $(function (){
 			flyLocation(locations[0]["longitude"], locations[0]["latitude"], locations[0]["zoom"], locations[0]["bearing"]);
 			$("tr").remove();
 			loadData(data[0], "id", "race", "time");
+			$(".sub-section").hide();
 		};
 	});
 
@@ -237,6 +278,7 @@ $(function (){
 			flyLocation(locations[myIndex]["longitude"], locations[myIndex]["latitude"], locations[myIndex]["zoom"], locations[myIndex]["bearing"]);
 			$("tr").remove();
 			loadData(data[myIndex], "id", "race", "time");
+			$(".sub-section").hide();
 
 		} else {
 			myIndex = 3;
@@ -244,17 +286,32 @@ $(function (){
 			flyLocation(locations[3]["longitude"], locations[3]["latitude"], locations[3]["zoom"], locations[3]["bearing"]);
 			$("tr").remove();
 			loadData(data[3], "id", "race", "time");
+			$(".sub-section").hide();
+
 		};
 	});	
 
-	// <ID MATCH CLICK>
+	// <ID MATCH (SUB-SECTION)>
 	$("tbody").on("click", "tr", function(){
 		for (var i = 0; i < data[myIndex].length; i++) {
 			if (this.id === data[myIndex][i]["id"]) {
-				console.log(data[myIndex][i]["pace"]);
-				console.log(data[myIndex][i]["overall"]);
+				$(".sub-section").show();
+				$("#raceVal").html(data[myIndex][i]["race"]);	
+				$("#finishVal").html(data[myIndex][i]["time"]);	
+				$("#paceVal").html(data[myIndex][i]["pace"]);	
+				$("#overallVal").html(data[myIndex][i]["overall"]);	
+				$("#gplaceVal").html(data[myIndex][i]["age-place"]);	
+				$("#gpercentVal").html(data[myIndex][i]["age-percentile"]);	
 			};
 		};
+	});
+
+	// CLOSE
+	$("#close").click(function(){
+		$(".sub-section").hide();
+	});
+	$(".main-overlay").click(function(){
+		$(".sub-section").hide();
 	});
 
 
@@ -262,7 +319,7 @@ $(function (){
 	$(document).keydown(function(e) {
         if (e.which === 37) {
         	$('#left-arrow').trigger('click');
-        } else if (e.which === 39) {
+        } else if (e.which === 39 || e.which === 32) {
         	$('#right-arrow').trigger('click');
         };
     });
